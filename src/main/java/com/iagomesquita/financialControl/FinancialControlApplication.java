7671c372dae95be33c973fe147672831770679d3
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-06-20 09:06:01 -0300

    first commit

---
+++ src/main/java/com/iagomesquita/financialControl/FinancialControlApplication.java
@@ -0,0 +1,13 @@
+package com.iagomesquita.financialControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iagomesquita/financialControl/FinancialControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iagomesquita.financialControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
