2eee8b24804ce1b9a8c77c36e94bdfadaf938cb2
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-06-22 12:13:24 -0300

    feat: add Get list Transaction

--- src/main/java/com/iagomesquita/financialControl/controller/TransactionController.java
+++ src/main/java/com/iagomesquita/financialControl/controller/TransactionController.java
@@ -4,9 +4,12 @@ import com.iagomesquita.financialControl.controller.Dto.TransactionCreationDto;
 import com.iagomesquita.financialControl.controller.Dto.TransactionDto;
 import com.iagomesquita.financialControl.model.entity.Transaction;
 import com.iagomesquita.financialControl.service.TransactionService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,4 +37,17 @@ public class TransactionController {
         TransactionDto.fromEntity(savedTransaction)
     );
   }
+
+  @GetMapping
+  public ResponseEntity<List<TransactionDto>> getAllTransactions() {
+    List<Transaction> transactionsDb = transactionService.getAllTransactions();
+    return ResponseEntity.ok(
+        transactionsDb
+            .stream()
+            .map(TransactionDto::fromEntity)
+            .toList()
+    );
+  }
+
+
 }
