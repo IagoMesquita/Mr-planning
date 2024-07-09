11c0fcb7558102ad8ed90bd4b288556983fa0c01
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-07-09 18:01:14 -0300

    feat: (New Learning) add JpaSpecificationExecutor

--- src/main/java/com/iagomesquita/financialControl/model/repository/TransactionRepository.java
+++ src/main/java/com/iagomesquita/financialControl/model/repository/TransactionRepository.java
@@ -4,11 +4,16 @@ import com.iagomesquita.financialControl.model.entity.Transaction;
 import com.iagomesquita.financialControl.model.enums.Type;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-  List<Transaction> findAllByType(Type typeTransaction);
-  List<Transaction> findAllByOrderByAmountDesc();
-  List<Transaction> findAllByOrderByDateDesc();
+public interface TransactionRepository extends JpaRepository<Transaction, Long>,
+    JpaSpecificationExecutor<Transaction> {
+
+//  List<Transaction> findAllByType(Type typeTransaction);
+//
+//  List<Transaction> findAllByOrderByAmountDesc();
+//
+//  List<Transaction> findAllByOrderByDateDesc();
 }
