a5fdb1005097e47b3ced66ac46f81220aba77cd2
Iago Mesquita <iagomesquitaalves@gmail.com>
2024-08-03 19:46:46 -0300

    feat: add NotFoundExpections

--- src/main/java/com/iagomesquita/financialControl/service/Exception/NotFoundExpection.java
+++ src/main/java/com/iagomesquita/financialControl/service/Exception/NotFoundExpection.java
@@ -1,6 +1,6 @@
 package com.iagomesquita.financialControl.service.Exception;
 
-public class NotFoundExpection extends  Exception {
+public class NotFoundExpection extends Exception {
   NotFoundExpection(String message) {
     super(message);
   }
--- src/main/java/com/iagomesquita/financialControl/service/Exception/UserNotFoundException.java
+++ src/main/java/com/iagomesquita/financialControl/service/Exception/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package com.iagomesquita.financialControl.service.Exception;
 
-public class UserNotFoundException extends NotFoundExpection{
+public class UserNotFoundException extends NotFoundExpection {
 
   public UserNotFoundException() {
     super("Usuário não encontrado!");
